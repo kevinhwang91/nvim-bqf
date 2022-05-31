a8a50a1ae6e905f77db16e55aa5f27f5cccff16c
kevinhwang91 <kevin.hwang@live.com>
2022-05-31 23:47:26 +0800

    fix(preview): cdo is broken

--- lua/bqf/preview/handler.lua
+++ lua/bqf/preview/handler.lua
@@ -227,6 +227,7 @@ function M.open(qwinid, qidx, force)
     pvs.floatWinExec(function()
         execPreview(item, lspRangeHl, patternHl)
         pvs.scroll(pbufnr, loaded)
+        cmd(('noa call nvim_set_current_win(%d)'):format(pwinid))
     end)
 end
 
